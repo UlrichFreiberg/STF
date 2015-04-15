a4f164f2dce0524278aeab2354ae2ddb9cb8f3dc
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-04-15 21:11:35 +0200

    Plugin interface for Unittest noved to seperate assembly

---
+++ UnitTest/StfPluginLoader/Stf.UnitTestPluginInterfaces/IStfUnitTestPlugin1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stf.Unittests
+{
+    using Stf.Utilities;
+
+    public interface IStfUnitTestPlugin1 : IStfPlugin
+    {
+        int StfUnitTestPlugin1Func();
+    }
+}
