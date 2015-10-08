13c53fa986d5c7280ea1b946c62f7abb8f1ebdf3
kaweera <kaweera@gmail.com>
2015-10-08 12:17:22 +0100

    Prepwork for Nuget package creation

---
+++ Nuget/Create-NugetPackage.ps1
@@ -0,0 +1,13 @@
+﻿$rootDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+$nugetPath = "{0}\nuget.exe" -f $rootDir
+$stfKernelPath = "{0}\..\Source\StfKernel" -f $rootDir
+
+Write-Host
+Write-Host "Note that this script requires Nuget.exe. Get it here: https://dist.nuget.org/index.html" 
+Write-Host
+
+Push-Location $stfKernelPath
+
+nuget pack StfKernel.csproj -IncludeReferencedProjects
+
+Pop-Location
