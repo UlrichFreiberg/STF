97dc72037e799dbc1e0264c4cf10f7a17189e15d
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-10-31 13:14:34 +0100

    StfDoc: Stf Description document and Demo solution

---
+++ Demos/DocDemo/DocDemo/DocDemoTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mir.Stf;
+
+namespace DocDemo
+{
+    [TestClass]
+    public class DocDemoTests : StfTestScriptBase
+    {
+        [TestMethod]
+        public void TestLogInfo()
+        {
+            MyLogger.LogInfo("Demo");
+            MyAssert.LessThan("Some Test Step", 5, 42);
+        }
+    }
+}
