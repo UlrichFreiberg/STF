12821cda9cfa1301c6bf6b9a6dc4b5887facabd8
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-11-01 23:28:33 +0100

    Demo : Added a unit test to show how the demo plugin should be used

---
+++ Demos/PluginDocDemo/UnitTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="Mir Software">
+//   Copyright governed by Artistic license as described here:
+//          http://www.perlfoundation.org/artistic_license_2_0
+// </copyright>
+// <summary>
+//   
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    using DemoCorp.Stf.PluginDocDemo.Interfaces;
+
+    using Mir.Stf;
+
+    /// <summary>
+    /// Unittests for the Plugin Demo
+    /// </summary>
+    [TestClass]
+    public class UnitTest1 : StfTestScriptBase
+    {
+        /// <summary>
+        /// Does it fly at all?
+        /// </summary>
+        [TestMethod]
+        public void TestCanInstantiate()
+        {
+            var pluginDocDemo = this.Get<IPluginDocDemo>();
+
+            pluginDocDemo.PrintValues();
+        }
+    }
+}
