84fd329da392bbed80e9889976f8da4bfd57804f
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-11-05 21:11:22 +0100

    SandBox : Pretty good shot at a LogTestOutputValue function

---
+++ SandBox/Ufr/StatePrinterDemo/LogTestOutputValues/LoggerForTestOutputValues.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogTestOutputValues
+{
+    using System.IO;
+
+    using StatePrinter;
+    using StatePrinter.Introspection;
+    using StatePrinter.OutputFormatters;
+
+    public class LoggerForTestOutputValues
+    {
+        private readonly Stateprinter printer;
+
+        private readonly CvsFormatter cvsFormatter;
+
+        public LoggerForTestOutputValues()
+        {
+            cvsFormatter = new CvsFormatter();
+            printer = new Stateprinter();
+            printer.Configuration.OutputFormatter = cvsFormatter;
+        }
+
+        public void LogTestOutputValues(object obj)
+        {
+            var cvsLine = printer.PrintObject(obj);
+
+            File.AppendAllText(LogFilename, cvsLine + Environment.NewLine);
+        }
+
+        public string LogFilename { get; set; }
+
+        public string FieldSeperator
+        {
+            get
+            {
+                return cvsFormatter.FieldSeperator;
+            }
+            set
+            {
+                cvsFormatter.FieldSeperator = value;
+            }
+        }
+
+        private class CvsFormatter : IOutputFormatter
+        {
+            public string FieldSeperator { get; set; }
+
+            public string Print(List<Token> tokens)
+            {
+                var retVal = tokens
+                    .Where(token => token.Field != null && token.Field.Name != null)
+                    .Aggregate(string.Empty, (current, token) => current + token.Value + FieldSeperator);
+
+                return retVal;
+            }
+        }
+    }
+}
---
+++ SandBox/Ufr/StatePrinterDemo/StatePrinterDemo/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StatePrinterDemo
+{
+    using LogTestOutputValues;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testObj1 = new { Col = 10, Name = "NameOfIt1", Row = 15 };
+            var testObj2 = new Test1 { Col = 20, Name = "NameOfIt2", Row = 25 };
+            var logger = new LoggerForTestOutputValues
+                             {
+                                 LogFilename = @"c:\temp\stf\temp\LoggerForTestOutputValues.csv"
+                             };
+
+            logger.LogTestOutputValues(testObj1);
+            logger.LogTestOutputValues(testObj2);
+        }
+    }
+
+
+    class Test1
+    {
+        public int Row;
+
+        public int Col;
+
+        public string Name;
+    }
+}
