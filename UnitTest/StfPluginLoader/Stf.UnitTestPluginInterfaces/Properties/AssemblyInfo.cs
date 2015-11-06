999f322d61b937ab9a7010347b559156b85c1506
kaweera <kaweera@gmail.com>
2015-11-06 09:14:01 +0100

    Updated versions after pushing nuget package

--- UnitTest/StfPluginLoader/Stf.UnitTestPluginInterfaces/Properties/AssemblyInfo.cs
+++ UnitTest/StfPluginLoader/Stf.UnitTestPluginInterfaces/Properties/AssemblyInfo.cs
@@ -32,8 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.44.51105")]
-[assembly: AssemblyFileVersion("1.0.44.51105")]
+[assembly: AssemblyVersion("1.0.45.51106")]
+[assembly: AssemblyFileVersion("1.0.45.51106")]
+
 
 
 
