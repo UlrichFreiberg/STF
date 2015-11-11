8fa7010b7f7b4b9000b1c2a2996314c05c4e3781
kaweera <kaweera@gmail.com>
2015-11-11 07:56:20 +0100

    Upped version after pushing Nuget package (pushed yesterday afternoon)

--- Source/StfPluginLoader/Properties/AssemblyInfo.cs
+++ Source/StfPluginLoader/Properties/AssemblyInfo.cs
@@ -51,8 +51,9 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.57.51110")]
-[assembly: AssemblyFileVersion("1.0.57.51110")]
+[assembly: AssemblyVersion("1.0.58.51110")]
+[assembly: AssemblyFileVersion("1.0.58.51110")]
+
 
 
 
