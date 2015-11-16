47e910e51f31e755c2e8639a85025aaea850c055
kaweera <kaweera@gmail.com>
2015-11-16 09:31:23 +0100

    Sandbox : Removed path wrapper from output

--- SandBox/Ufr/CurrentDirectoryTest/CurrentDirectoryTest/UnitTest1.cs
+++ SandBox/Ufr/CurrentDirectoryTest/CurrentDirectoryTest/UnitTest1.cs
@@ -26,28 +26,28 @@ namespace CurrentDirectoryTest
             var retVal = string.Empty;
             var assemblyGetEntryAssembly = Assembly.GetEntryAssembly();
 
-            retVal = addOneFunctionResult(retVal, "Environment.CurrentDirectory", Path.GetDirectoryName(Environment.CurrentDirectory));
-            retVal = addOneFunctionResult(retVal, "Directory.GetCurrentDirectory()", Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+            retVal = addOneFunctionResult(retVal, "Environment.CurrentDirectory", Environment.CurrentDirectory);
+            retVal = addOneFunctionResult(retVal, "Directory.GetCurrentDirectory()", Directory.GetCurrentDirectory());
 
             if (assemblyGetEntryAssembly != null)
             {
-                retVal = addOneFunctionResult(retVal, "getEntryAssembly.Location", Path.GetDirectoryName(assemblyGetEntryAssembly.Location));
-                retVal = addOneFunctionResult(retVal, "new Uri(getEntryAssembly.CodeBase).LocalPath", Path.GetDirectoryName(new Uri(assemblyGetEntryAssembly.CodeBase).LocalPath));
+                retVal = addOneFunctionResult(retVal, "getEntryAssembly.Location", assemblyGetEntryAssembly.Location);
+                retVal = addOneFunctionResult(retVal, "new Uri(getEntryAssembly.CodeBase).LocalPath", new Uri(assemblyGetEntryAssembly.CodeBase).LocalPath);
             }
 
-            retVal = addOneFunctionResult(retVal, "Environment.GetCommandLineArgs()[0]", Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]));
+            retVal = addOneFunctionResult(retVal, "Environment.GetCommandLineArgs()[0]", Environment.GetCommandLineArgs()[0]);
             retVal = addOneFunctionResult(retVal, "Process.GetCurrentProcess().MainModule.FileName", Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName));
 
             if (TestContext != null)
             {
-                retVal = addOneFunctionResult(retVal, "TestContext.ResultsDirectory", Path.GetDirectoryName(TestContext.ResultsDirectory));
-                retVal = addOneFunctionResult(retVal, "TestContext.DeploymentDirectory", Path.GetDirectoryName(TestContext.DeploymentDirectory));
-                retVal = addOneFunctionResult(retVal, "TestContext.TestResultsDirectory", Path.GetDirectoryName(TestContext.TestResultsDirectory));
-                retVal = addOneFunctionResult(retVal, "TestContext.TestRunDirectory", Path.GetDirectoryName(TestContext.TestRunDirectory));
-                retVal = addOneFunctionResult(retVal, "TestContext.TestRunResultsDirectory", Path.GetDirectoryName(TestContext.TestRunResultsDirectory));
+                retVal = addOneFunctionResult(retVal, "TestContext.ResultsDirectory", TestContext.ResultsDirectory);
+                retVal = addOneFunctionResult(retVal, "TestContext.DeploymentDirectory", TestContext.DeploymentDirectory);
+                retVal = addOneFunctionResult(retVal, "TestContext.TestResultsDirectory", TestContext.TestResultsDirectory);
+                retVal = addOneFunctionResult(retVal, "TestContext.TestRunDirectory", TestContext.TestRunDirectory);
+                retVal = addOneFunctionResult(retVal, "TestContext.TestRunResultsDirectory", TestContext.TestRunResultsDirectory);
             }
 
-            retVal = addOneFunctionResult(retVal, "AppDomain.CurrentDomain.BaseDirectory", Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory));
+            retVal = addOneFunctionResult(retVal, "AppDomain.CurrentDomain.BaseDirectory", AppDomain.CurrentDomain.BaseDirectory);
             // retVal = addOneFunctionResult(retVal, "Application.StartupPath", Path.GetDirectoryName(Application.StartupPath));
             // retVal = addOneFunctionResult(retVal, "Application.ExecutablePath", Path.GetDirectoryName(Application.ExecutablePath));
 
