ceaee007fc942838426f52a41d569e77c9c99e09
kaweera <kaweera@gmail.com>
2015-11-16 10:52:48 +0100

    Pushed new nuget package

--- Source/StfAssert/Properties/AssemblyInfo.cs
+++ Source/StfAssert/Properties/AssemblyInfo.cs
@@ -35,8 +35,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.61.51113")]
-[assembly: AssemblyFileVersion("1.0.61.51113")]
+[assembly: AssemblyVersion("1.0.62.51116")]
+[assembly: AssemblyFileVersion("1.0.62.51116")]
+
 
 
 
