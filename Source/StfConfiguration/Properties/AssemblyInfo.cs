d3eb18ca169a68803a38ae095df574499520b422
kaweera <kaweera@gmail.com>
2015-12-03 09:34:06 +0100

    Pushed new package to nuget

--- Source/StfConfiguration/Properties/AssemblyInfo.cs
+++ Source/StfConfiguration/Properties/AssemblyInfo.cs
@@ -39,8 +39,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.69.51127")]
-[assembly: AssemblyFileVersion("1.0.69.51127")]
+[assembly: AssemblyVersion("1.0.71.51203")]
+[assembly: AssemblyFileVersion("1.0.71.51203")]
+
+
 
 
 
--- Source/StfPluginLoader/Properties/AssemblyInfo.cs
+++ Source/StfPluginLoader/Properties/AssemblyInfo.cs
@@ -51,8 +51,10 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.69.51127")]
-[assembly: AssemblyFileVersion("1.0.69.51127")]
+[assembly: AssemblyVersion("1.0.71.51203")]
+[assembly: AssemblyFileVersion("1.0.71.51203")]
+
+
 
 
 
