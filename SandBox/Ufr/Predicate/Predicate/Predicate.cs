473424c7b4826bf4b886249c13120b4290e55e51
Ulrich Freiberg <Ulrich@mirsoftware.com>
2016-01-12 22:41:08 +0100

    Sandbox: Predicate class extended a bit

--- SandBox/Ufr/Predicate/Predicate/Predicate.cs
+++ SandBox/Ufr/Predicate/Predicate/Predicate.cs
@@ -16,21 +16,61 @@ namespace Predicate
     using System.Linq;
     using System.Linq.Dynamic;
 
+    /// <summary>
+    /// Utility class to handle predicates towards a list
+    /// </summary>
     public class Predicate
     {
+        /// <summary>
+        /// </summary>
+        /// <param name="predicateList">
+        /// </param>
+        public Predicate(string predicateList = null)
+        {
+            PredicateList = predicateList;
+        }
+
+        /// <summary>
+        /// Gets or sets the full predicate
+        /// </summary>
         public string PredicateList { get; set; }
 
+        /// <summary>
+        /// Override of ToString
+        /// </summary>
+        /// <returns>
+        /// The predicate list
+        /// </returns>
         public override string ToString()
         {
             return PredicateList;
         }
 
+        /// <summary>
+        /// Filter the list according to the filter object
+        /// </summary>
+        /// <param name="listOfEntities">
+        /// The list to filter
+        /// </param>
+        /// <param name="filterClass">
+        /// The object that contains the filter - one property per predicate
+        /// </param>
+        /// <typeparam name="TEntity">
+        /// The Type for the entities in the list to filter
+        /// </typeparam>
+        /// <typeparam name="TFilter">
+        /// The object that contains the filter
+        /// </typeparam>
+        /// <returns>
+        /// The filtered list
+        /// </returns>
         public List<TEntity> FilterList<TEntity, TFilter>(List<TEntity> listOfEntities, TFilter filterClass)
-            where TEntity : new() where TFilter : TEntity
+            where TEntity : new() 
+            where TFilter : TEntity
         {
-            // foreach property in filterClass
-            //   if property is not null
-            //     filter according to property
+            //// foreach property in filterClass
+            ////   if property is not null
+            ////     filter according to property
 
             var retVal = listOfEntities;
             var properties = typeof(TFilter).GetProperties();
@@ -73,6 +113,66 @@ namespace Predicate
             }
 
             return retVal;
+        }
+
+        /// <summary>
+        /// Returns a Filter initialized accordingly to a string predicate 
+        /// </summary>
+        /// <param name="predicate">
+        /// The predicate
+        /// </param>
+        /// <typeparam name="TFilter">
+        /// The filter class
+        /// </typeparam>
+        /// <returns>
+        /// A initialized filter
+        /// </returns>
+        public TFilter ParsePredicate<TFilter>(string predicate) where TFilter : new()
+        {
+            //// foreach predicate in the predicate list
+            ////     find the property in filterClass
+            ////     set the filtvalue to the predicate value part
+             
+            // TODO: Implement:-)
+            return new TFilter();
+        }
+
+        /// <summary>
+        /// Enumerate all the predicate parts of the overall predicate
+        /// </summary>
+        /// <returns>
+        /// A predicate part
+        /// </returns>
+        public IEnumerable<PredicatePart> Predicates()
+        {
+            if (!string.IsNullOrEmpty(PredicateList))
+            {
+                var predicates = PredicateList.Split(';');
+
+                foreach (var predicate in predicates)
+                {
+                    var retVal = new PredicatePart(predicate);
+
+                    yield return retVal;
+                }
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="filter">
+        /// </param>
+        /// <typeparam name="TFilter">
+        /// </typeparam>
+        /// <returns>
+        /// </returns>
+        public string GeneratePredicate<TFilter>(TFilter filter)
+        {
+            //// foreach nullable property with a value in filter 
+            ////     Generate a predicate part "LHS = RHS"
+
+            // TODO: Implement:-)
+            return "NeedsToBeImplemented";            
         }
     }
 }
---
+++ SandBox/Ufr/Predicate/Predicate/PredicatePart.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PredicatePart.cs" company="Mir Software">
+//   Copyright governed by Artistic license as described here:
+//          http://www.perlfoundation.org/artistic_license_2_0
+// </copyright>
+// <summary>
+//   A predicate consists of parts of predicates seperated by a seperator. Like:
+//      OrderType = "Spot" ; OrderPrice = 56
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Predicate
+{
+    /// <summary>
+    /// Class to handle parts of a predicate
+    /// </summary>
+    public class PredicatePart
+    {
+        /// <summary>
+        /// The predicate string
+        /// </summary>
+        private string predicate;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="predicate">
+        /// </param>
+        public PredicatePart(string predicate)
+        {
+            this.predicate = predicate;
+            var tokens = predicate.Split('=');
+
+            Quantifier = tokens.Length <= 0 ? string.Empty : tokens[0];
+            Value = tokens.Length <= 1 ? string.Empty : tokens[1];
+        }
+
+        /// <summary>
+        /// Gets or sets the Left Hand side of the predicate
+        /// </summary>
+        public string Quantifier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Right Hand side of the predicate
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
