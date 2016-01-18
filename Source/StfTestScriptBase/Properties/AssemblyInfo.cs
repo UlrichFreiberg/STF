b0a041c3b0b32e90f7100d09c3bfb667c7bf8366
kaweera <kaweera@gmail.com>
2016-01-18 22:13:17 +0100

    Upped version after pushing nuget package

--- Source/StfTestScriptBase/Properties/AssemblyInfo.cs
+++ Source/StfTestScriptBase/Properties/AssemblyInfo.cs
@@ -32,8 +32,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.71.51203")]
-[assembly: AssemblyFileVersion("1.0.71.51203")]
+[assembly: AssemblyVersion("1.0.74.60118")]
+[assembly: AssemblyFileVersion("1.0.74.60118")]
+
+
+
 
 
 
