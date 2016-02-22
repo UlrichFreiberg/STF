73f29125672ff92181a2c26d748b1bf9230f186f
Ulrich Freiberg <Ulrich@mirsoftware.com>
2016-02-22 09:04:36 +0100

    Adding LogLevel and environment to TestDataBase

--- SandBox/Ufr/DataDriven/DataDriven/StfTestDataBase.cs
+++ SandBox/Ufr/DataDriven/DataDriven/StfTestDataBase.cs
@@ -20,5 +20,11 @@ namespace DataDriven
         /// Useful when debugging the 50th data row:-)
         /// </summary>
         public bool IgnoreRow { get; set; }
+
+        // some rows might demand more logging than others
+        public string LogLevel { get; set; }
+
+        // For suites that does the same in many environments (used for smoketests in test environments)
+        public string Environment { get; set; }
     }
 }
