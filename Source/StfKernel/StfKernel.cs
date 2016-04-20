f4e49a957fab15b9dd37ebfc7aceb0dab52b7406
Ulrich Freiberg <Ulrich@mirsoftware.com>
2016-04-20 08:09:52 +0200

    Fixed the order of the (Machine)Configuration.xml files...

--- Source/StfKernel/StfKernel.cs
+++ Source/StfKernel/StfKernel.cs
@@ -48,13 +48,15 @@ namespace Mir.Stf
 
             KernelLogger = new StfLogger(kernelLoggerConfiguration);
 
-            // get the configuration together
-            AssembleStfConfiguration();
+            // get the initial configuration together
+            AssembleStfConfigurationBeforePlugins();
 
             // Any plugins for us?
             PluginLoader = new StfPluginLoader(KernelLogger, StfConfiguration);
             PluginLoader.RegisterInstance(typeof(StfConfiguration), StfConfiguration);
             PluginLoader.LoadStfPlugins(StfConfiguration.GetKeyValue("StfKernel.PluginPath"));
+
+            AssembleStfConfigurationAfterPlugins();
 
             // now all configurations are loaded, we can set the Environment.
             StfConfiguration.Environment = StfConfiguration.DefaultEnvironment;
@@ -187,10 +189,7 @@ namespace Mir.Stf
                         KernelLogger.CloseLogFile();
                     }
 
-                    if (PluginLoader != null)
-                    {
-                        PluginLoader.Dispose();
-                    }
+                    PluginLoader?.Dispose();
                 }
 
                 disposed = true;
@@ -226,19 +225,20 @@ namespace Mir.Stf
         /// <summary>
         /// The assembly stf configuration.
         /// </summary>
-        private void AssembleStfConfiguration()
+        private void AssembleStfConfigurationBeforePlugins()
         {
             var stfConfigurationFile = Path.Combine(StfConfigDir, @"StfConfiguration.xml");
 
-            if (File.Exists(stfConfigurationFile))
-            {
-                StfConfiguration = new StfConfiguration(stfConfigurationFile);
-            }
-            else
-            {
-                StfConfiguration = new StfConfiguration();
-            }
+            StfConfiguration = File.Exists(stfConfigurationFile)
+                ? new StfConfiguration(stfConfigurationFile)
+                : new StfConfiguration();
+        }
 
+        /// <summary>
+        /// The assembly stf configuration.
+        /// </summary>
+        private void AssembleStfConfigurationAfterPlugins()
+        {
             OverlayStfConfigurationForOneSettingType(StfConfigDir, ConfigurationFileType.Machine);
             OverlayStfConfiguration(Environment.CurrentDirectory);
         }
