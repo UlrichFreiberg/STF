ae261b7fb95e8eb520a51475ef5a7ff6a0740471
kaweera <kaweera@gmail.com>
2016-06-29 07:31:59 +0100

    Pushed new nuget package

--- Source/StfLogger/Properties/AssemblyInfo.cs
+++ Source/StfLogger/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.136.60616")]
-[assembly: AssemblyFileVersion("1.0.136.60616")]
+[assembly: AssemblyVersion("1.0.137.60629")]
+[assembly: AssemblyFileVersion("1.0.137.60629")]
