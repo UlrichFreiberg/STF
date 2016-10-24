c68fd653aa8d10aaff739452fda0dd0718dd395a
Ulrich Freiberg <Ulrich@mirsoftware.com>
2016-10-24 13:31:15 +0200

    Relesed STF 1.0.149

--- Source/StfKernel/Properties/AssemblyInfo.cs
+++ Source/StfKernel/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.148.61018")]
-[assembly: AssemblyFileVersion("1.0.148.61018")]
+[assembly: AssemblyVersion("1.0.149.61024")]
+[assembly: AssemblyFileVersion("1.0.149.61024")]
