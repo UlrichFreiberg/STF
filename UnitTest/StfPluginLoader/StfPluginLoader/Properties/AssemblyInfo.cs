c4ad541e9d981e95e3000dcd99523bf313b1642c
kaweera <kaweera@gmail.com>
2017-02-01 21:06:47 +0100

    Updated version to 1.0.155.02017

--- UnitTest/StfPluginLoader/StfPluginLoader/Properties/AssemblyInfo.cs
+++ UnitTest/StfPluginLoader/StfPluginLoader/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.153.01307")]
-[assembly: AssemblyFileVersion("1.0.153.01307")]
+[assembly: AssemblyVersion("1.0.155.02017")]
+[assembly: AssemblyFileVersion("1.0.155.02017")]
